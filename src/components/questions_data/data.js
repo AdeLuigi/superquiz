3637c10b61c678140dee7d62c4ccf21cb2196199
Ademario Vitor <ademario63@gmail.com>
2021-10-24 23:21:32 -0300

    não aguento mais Algebra linear

--- src/components/questions_data/data.js
+++ src/components/questions_data/data.js
@@ -38,5 +38,75 @@ export default [
             'Máquina Bombe'
         ],    
         correct: 1
+    },
+    {
+        question: 'Em qual país a máquina enigma foi construida ?',
+        answers: [
+            'Alemanha',
+            'Holanda', 
+            'Japão', 
+            'Espanha'
+        ],    
+        correct: 1
+    },
+    {
+        question: 'Qual era o nome da máquina que Alan construiu para decifrar as mensagens da Enigma ?',
+        answers: [
+            'Bomba',
+            'A Máquina Enigma', 
+            'Bombe', 
+            'Máquina de costura'
+        ],    
+        correct: 3
+    },
+    {
+        question: 'Como Alan Turing contribuiu para a vitória dos aliados na Segunda Guerra Mundial ?',
+        answers: [
+            'Fazendo cálculos matemáticos',
+            'Com a construção de uma máquina capaz de decifrar códigos da Enigma', 
+            'Sendo professor em uma universidade', 
+            'Estudando criptologia'
+        ],    
+        correct: 2
+    },
+    {
+        question: 'Qual esporte Turing praticava quando era estudante universitário ?',
+        answers: [
+            'Corrida',
+            'Futebol', 
+            'Natação', 
+            'Basquete'
+        ],    
+        correct: 1
+    },
+    {
+        question: 'Qual foi o ano e a melhor distância percorrida por Turing em 2 horas, 46 minutos e 3 segundos ?',
+        answers: [
+            '34 km - 1946',
+            '42 km - 1947', 
+            '45 km - 1948', 
+            '26 km - 1949'
+        ],    
+        correct: 2
+    },
+    {
+        question: 'Em qual das áreas abaixo, Alan Turing não fez contribuição ?',
+        answers: [
+            'Teoria da Computação',
+            'Computação Concorrente', 
+            'Redes Neurais', 
+            'Bioinformática'
+        ],    
+        correct: 2
+    },
+    {
+        question: 'Qual foi o motivo e ano de falecimento de Alan Turing ?',
+        answers: [
+            'Morte natural - 1952',
+            'Infarto - 1953', 
+            'Suicídio - 1954', 
+            'Câncer - 1955'
+        ],    
+        correct: 3
     }
 ]
