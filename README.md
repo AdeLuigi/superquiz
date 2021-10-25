ffcfa2084ea2846c0315bc132fc6ab8fde266ebf
Ademario Vitor <ademario63@gmail.com>
2021-10-24 23:11:21 -0300

    sd

---
+++ README.md
@@ -0,0 +1,19 @@
+#  Quiz sobre Alan Turing
+
+## 📝 Uso
+
+### Clone this Repo
+
+### Installation
+
+```sh
+$ npm install
+```
+
+### ✔️ Run
+
+Then go to http://localhost:8000
+
+```sh
+$ npm start
+```
---
+++ src/components/questions_data/data.js
@@ -0,0 +1,42 @@
+export default [
+    {
+        question: 'Em que país e ano Alan Turing nasceu ?',
+        answers: [
+            'Estados Unidos - 1910',
+            'Espanha - 1911',
+            'Inglaterra - 1912',
+            'África do Sul - 1913'
+        ],
+        correct: 3
+    },
+    {
+        question: 'Em qual universidade Alan Turing se graduou ?',
+        answers: [
+            'Universidade de Cambridge',
+            'Universidade de Stanford',
+            'Universidade Harvard',
+            'Universidade de Oxford'
+        ],
+        correct: 1
+    },    
+    {
+        question: 'Qual ponto da teoria da computação está mais próximo de um computador ?',
+        answers: [
+            'Um “procedimento mecânico”(algoritmo) é uma tarefa que pode ser executada por uma Máquina de Turing',
+            'Máquina de Turing Universal capaz de simular o funcionamento de qualquer outra máquina de Turing', 
+            'Existência de problemas que não são algoritmicamente solúveis, tal como o Problema da Parada', 
+            'O modelo de Máquinas de Turing é equivalente ao de Alonso Church.'
+        ],    
+        correct: 2
+    },    
+    {
+        question: 'Em seu doutorado, o que Alan turing construiu ?',
+        answers: [
+            'Máquina de cifras baseada em um mulplicador binário',
+            'Máquina universal de Turing', 
+            'Máquina para fazer análises de criptologia', 
+            'Máquina Bombe'
+        ],    
+        correct: 1
+    }
+]
